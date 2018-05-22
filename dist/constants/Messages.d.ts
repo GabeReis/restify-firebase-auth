837219e0da6d26b7bb47c84aed5737f51dc88f9a
John Reis <jgabreis@gmail.com>
2018-05-22 12:05:55 -0700

    fix(repo): fixing merging issues

--- dist/constants/Messages.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const UNAUTHORIZED_ERROR_NO_AUTHORIZATION = "No authorization token was not found";
-export declare const UNAUTHORIZED_ERROR_CREDENTIALS_BAD_FORMAT = "Credential format is unknown - Use: Bearer [token]";
-export declare const UNAUTHORIZED_ERROR_INVALID_TOKEN = "Invalid token";
-export declare const AUTHORIZED_MESSAGE = "Access authorized";
